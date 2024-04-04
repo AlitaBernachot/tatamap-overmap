89d748192912c35c06548ff6ae97ef26132dd3b1
AlitaBernachot <alita.bernachot@camptocamp.com>
2024-04-04 11:38:38 +0200

    feat: add vuetify

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,88 +1,74 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import { ref } from 'vue'
+
+const variants = ['elevated', 'flat', 'tonal', 'outlined']
+const color = ref('indigo')
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col
+        cols="auto"
+      >
+        <v-radio-group
+          v-model="color"
+          inline
+        >
+          <v-radio
+            color="indigo"
+            label="indigo"
+            value="indigo"
+          ></v-radio>
+          <v-radio
+            color="indigo-darken-3"
+            label="indigo-darken-3"
+            value="indigo-darken-3"
+          ></v-radio>
+          <v-radio
+            color="primary"
+            label="primary"
+            value="primary"
+          ></v-radio>
+          <v-radio
+            color="secondary"
+            label="secondary"
+            value="secondary"
+          ></v-radio>
+        </v-radio-group>
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center">
+      <v-col
+        v-for="(variant, i) in variants"
+        :key="i"
+        cols="auto"
+      >
+        <v-card
+          :color="color"
+          :variant="variant"
+          class="mx-auto"
+          max-width="344"
+        >
+          <v-card-item>
+            <div>
+              <div class="text-overline mb-1">
+                {{ variant }}
+              </div>
+              <div class="text-h6 mb-1">
+                Headline
+              </div>
+              <div class="text-caption">Greyhound divisely hello coldly fonwderfully</div>
+            </div>
+          </v-card-item>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+          <v-card-actions>
+            <v-btn>
+              Button
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
-import { createI18n } from 'vue-i18n'
+
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 const i18n = createI18n({
     // something vue-i18n options here ...
@@ -14,7 +24,8 @@ const i18n = createI18n({
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 app.use(i18n)
+app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
